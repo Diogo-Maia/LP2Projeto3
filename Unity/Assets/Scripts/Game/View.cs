d44d0050e92310f19ee1e07ea1c4aa24f04ca6e1
Diogo-Maia <Diogo_Maia99@hotmail.com>
2021-01-29 15:06:36 +0000

    Starting working on Unity

---
+++ Unity/Assets/Scripts/Game/View.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using Common.Files;
+
+public class View : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameManager gm = new GameManager();
+
+        gm.GameLoop();
+    }
+}
